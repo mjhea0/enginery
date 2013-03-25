d5dede8b59fa43a706233626e671b442fa5cc2c7
slivu <slivuz@gmail.com>
2013-03-25 17:09:53 +0200

    refactoring/fixing database.rb

--- app/database/ActiveRecord.rb
+++ app/database/ActiveRecord.rb
@@ -1,7 +1,7 @@
-
 if Cfg.db[:type] && Cfg.db[:name]
-  values, name = Cfg.db.values_at(:type, :user, :pass, :host), Cfg.db[:name]
-  (Cfg.db[:type].to_s =~ /sqlite/i) && (name = Cfg.root_path(name))
-  connection_string = "%s://%s:%s@%s/#{name}" % values
-  ActiveRecord::Base.establish_connection connection_string
+  url = '%s:%s@%s/%s' % Cfg.db.values_at(:user, :pass, :host, :name)
+  if Cfg.db[:type] =~ /sqlite/i
+    url = Cfg.db[:name] =~ /\A\// ? Cfg.db[:name] : Cfg.root_path(Cfg.db[:name])
+  end
+  ActiveRecord::Base.establish_connection '%s://%s' % [Cfg.db[:type], url]
 end
--- app/database/Sequel.rb
+++ app/database/Sequel.rb
@@ -1,7 +1,6 @@
-
 if Cfg.db[:type] && Cfg.db[:name]
-  if Cfg.db[:type].to_s =~ /sqlite/i
-    DB = Sequel.sqlite Cfg.root_path(Cfg.db[:name])
+  if Cfg.db[:type] =~ /sqlite/i
+    DB = Sequel.sqlite Cfg.db[:name] =~ /\A\// ? Cfg.db[:name] : Cfg.root_path(Cfg.db[:name])
   else
     DB = Sequel.connect "%s://%s:%s@%s/%s" % Cfg.db.values_at(:type, :user, :pass, :host, :name)
   end
